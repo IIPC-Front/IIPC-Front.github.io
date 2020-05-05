5b1f474d4568e56ffe8d67459e14c4c15ac8d3c4
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-05-05 23:08:21 +0530

    added responsivity removed some carousel properties

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,3 +4,13 @@ $('.main-gallery').flickity({
     contain: true
   });
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+
+
