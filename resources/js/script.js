3f2555b63968e98895e4e4e60939aa1a4db6b35e
https://github.com/DeathNaughT-GitHub <coding.ayush@gmail.com>
2020-05-08 01:34:30 +0530

    layouts ready for all to start

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,13 +4,13 @@ $('.main-gallery').flickity({
     contain: true
   });
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-} 
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+} 
 
